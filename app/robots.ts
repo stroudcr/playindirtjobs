3c256b7648111b8750f5abc4468d9a65c552e2fa
stroudcr <stroudcr@gmail.com>
2025-10-30 21:26:40 -0400

    SEO Update 1

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,38 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/post-job/preview',
+        ],
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/post-job/preview',
+        ],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/post-job/preview',
+        ],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/post-job/preview',
+        ],
+      },
+      {
         userAgent: '*',
         allow: '/',
         disallow: [
